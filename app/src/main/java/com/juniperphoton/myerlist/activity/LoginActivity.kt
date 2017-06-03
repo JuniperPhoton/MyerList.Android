22a3a8dbde6fd652b9c1827bb5706d5835a2c75e
JuniperPhoton <dengweichao@hotmail.com>
2017-06-03 22:10:59 +0800

    Improve input experience in login activity

--- app/src/main/java/com/juniperphoton/myerlist/activity/LoginActivity.kt
+++ app/src/main/java/com/juniperphoton/myerlist/activity/LoginActivity.kt
@@ -4,12 +4,16 @@ import android.app.ProgressDialog
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.TextView
 import butterknife.ButterKnife
 import butterknife.OnClick
 import com.juniperphoton.myerlist.BuildConfig
 import com.juniperphoton.myerlist.R
 import com.juniperphoton.myerlist.presenter.LoginPresenter
+import com.juniperphoton.myerlist.util.KeyboardUtil
 import com.juniperphoton.myerlist.util.Params
+import com.juniperphoton.myerlist.util.getResString
 import com.juniperphoton.myerlist.view.LoginView
 import kotlinx.android.synthetic.main.activity_login.*
 
@@ -33,21 +37,32 @@ class LoginActivity : BaseActivity(), LoginView {
         loginMode = intent.getIntExtra(Params.LOGIN_MODE, -1)
         when (loginMode) {
             Params.LoginMode.LOGIN -> {
-                loginTitleView.text = getString(R.string.login_title)
+                loginTitleView.text = R.string.login_title.getResString()
                 passwordSecViewRoot.visibility = View.GONE
-                loginButton.text = getString(R.string.login)
+                loginButton.text = R.string.login.getResString()
             }
             Params.LoginMode.REGISTER -> {
-                loginTitleView.text = getString(R.string.register_title)
+                loginTitleView.text = R.string.register_title.getResString()
                 passwordSecViewRoot.visibility = View.VISIBLE
-                loginButton.text = getString(R.string.register)
+                loginButton.text = R.string.register.getResString()
             }
         }
         if (BuildConfig.DEBUG) {
-            emailView.setText("dengweichao@hotmail.com")
-            passwordView.setText("test");
-            //passwordSecView.setText("test");
+            emailView.setText(R.string.email_dev.getResString())
         }
+
+        val listener = TextView.OnEditorActionListener handle@ { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_NEXT) {
+                KeyboardUtil.hide(this, passwordView.windowToken)
+                onClickButton()
+                return@handle true
+            }
+            return@handle false
+        }
+
+        passwordView.setOnEditorActionListener(listener)
+        passwordSecView.setOnEditorActionListener(listener)
+
         presenter = LoginPresenter(this, loginMode)
     }
 
@@ -68,8 +83,8 @@ class LoginActivity : BaseActivity(), LoginView {
     @OnClick(R.id.loginButton)
     fun onClickButton() {
         dialog = ProgressDialog(this, ProgressDialog.STYLE_SPINNER)
-        dialog!!.setTitle(getString(R.string.loading_hint))
-        dialog!!.setMessage(getString(R.string.waiting))
+        dialog!!.setTitle(R.string.loading_hint.getResString())
+        dialog!!.setMessage(R.string.waiting.getResString())
         dialog!!.setCancelable(false)
         dialog!!.show()
         if (loginMode == Params.LoginMode.LOGIN) {
